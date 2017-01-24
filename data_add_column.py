85bd598d27d0b97ce2d3d9c32233fd6af3d0c69f
TalitaAnthonio <Stamper1>
2017-01-24 20:13:29 +0100

    Added csv file, generated with the script data_add_column.py. New file is exactly the same as the original file, except for the columns pos_count and neg_count. neg_count = the number of negative bigrams, pos_count = the number of positive bigrams.

--- data_add_column.py
+++ data_add_column.py
@@ -1,40 +1,74 @@
 import csv
 import sys
+from nltk import bigrams, word_tokenize
+from pprint import pprint
 
 
-positive_words = ["Don't", "I", "That", "she"]
-negative_words = ["Forced", "after", "most"]
+def make_list(text):
+    """
+        A function to sort lists in order to search faster
+    """
+    word_list = []
+    for words in text:
+        words = words.replace("\n","").split(" ")
+        word_list.append((words[0], words[1]))
+    return sorted(word_list)
 
 
-def extract_text(data):
-    for row in data:
-        text = row['text-cat']
-        for positive_bigrams in positive_words:
-            if positive_bigrams in text:
-                print('yes, positive')
-        for negative_bigrams in negative_words:
-            if negative_bigrams in text:
-                print('yes, negative')
+def row_to_bigrams(row):
+    sentence_bigrams = [bigram for bigram in bigrams(word_tokenize(row["text-cat"])) if bigram]
+    return sentence_bigrams
 
-def remove_things(file):
-    for line in file:
-        line = line.strip()
-        print("".join(line))
+
+def score_text(p_sentiment_list, n_sentiment_list, lines):
+    scores = []
+    for line in lines:
+        bigram_list = row_to_bigrams(line)
+        p_score_line = intersection(p_sentiment_list, bigram_list)
+        n_score_line = intersection(n_sentiment_list, bigram_list)
+        scores.append(
+            {
+                "label": line["label"],
+                "text-cat": line["text-cat"],
+                "pos_score": p_score_line,
+                "neg_score": n_score_line
+            }
+        )
+    return scores
+
+def intersection(sentiment_list, sentence_bigrams):
+    score = 0
+    for sentiment_bigram in sentiment_list:
+        score_delta = sentence_bigrams.count(sentiment_bigram)
+        score += score_delta
+    return score
+
 
 def main():
+    # open all the files
     reload(sys)
-    file = open('trainset-sentiment.csv')
+    file = open('trainset-sentiment-test.csv')
     sys.setdefaultencoding("utf-8")
     reader = csv.DictReader(file)
-    extract_text(reader)
+
+    positive_file = open('pos_bigrams.txt')
+    negative_file = open('neg_bigrams.txt')
+
+    list1 = make_list(positive_file)
+    list2 = make_list(negative_file)
+    scores = score_text(list1, list2, reader)
+
+    headers = ["label","text-cat","pos_score","neg_score"]
+    out_file = open("out.csv", 'w')
+    csv_out = csv.DictWriter(f=out_file, fieldnames=headers)
+    csv_out.writeheader()
+    for score in scores:
+        csv_out.writerow(score)
+
     file.close()
-
-    neg = open('neg_bigrams.txt')
-    remove_things(neg)
-    neg.close()
-
-    #pos = open('pos_bigrams.txt')
-    #pos.close()
+    positive_file.close()
+    negative_file.close()
+    out_file.close()
 
 main()
 
